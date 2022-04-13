7a56c96ffc4e3803efa7cfad15a472623d3010b2
Chung Kao <chungckao@gmail.com>
2022-04-14 00:24:03 +0800

    Edit hangman.py to make it more lively

--- hangman.rb
+++ hangman.rb
@@ -1,18 +1,22 @@
-# Hardcode solutionds
-def hangman(word, guess)
-  word.gsub(/[^#{guess.join('')}]/, "_")
-end
-
-puts hangman('hangman', ['h', 'g'])
-
-# Interactive solution
 def hangman(word)
-  guess = []
-  until guess.last == ""
-    print "Guess a letter in the word, or hit enter to see the result: "
-    guess << gets.chomp
+  guesses = ''
+  until guesses == ' '
+    print 'Guess a letter in the word, or press enter to quit: '
+    guess = gets.chomp
+    if guess.empty?
+      guesses = ' '
+    elsif guess.match?(/[^A_Za-z]/)
+      puts "\nYou must enter a letter. Try again.\n\n"
+    else
+      guesses = guesses.include?(guess) ? guesses : guesses + guess
+      if word.gsub(/[#{guesses}]/, '') == ''
+        puts "\nYou won: #{word}"
+        guesses = ' '
+      else
+        puts "\n#{word.include?(guess) ? 'Hit:' : 'Miss:'} #{word.gsub(/[^#{guesses}]/, '_')}\n\n"
+      end
+    end
   end
-  word.gsub(/[^#{guess.join('')}]/, "_")
 end
 
-puts hangman('hangman')
+hangman('hangman')
